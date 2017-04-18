1a718355a7b9a0a3cf8cb89bc713d103d33f5e1a
Brendon <btucker1992@gmail.com>
2017-04-18 18:29:40 +0800

    started work here again, need more OOP practice

---
+++ Ch8Again.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 18 16:48:13 2017
+
+@author: brendontucker
+"""
+
+def __init__(self):
+    self.vals = []
+    
+def insert(self, e):
+    if not e in self.vals:
+        self.vals.append(e)
