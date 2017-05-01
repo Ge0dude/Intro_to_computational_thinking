4b45d736671157fc678619e984101368cd5b3237
Brendon <btucker1992@gmail.com>
2017-05-01 12:08:40 +0800

    more OOP

--- CH8/peopleDB.py
+++ CH8/peopleDB.py
@@ -49,5 +49,52 @@ class Person(object):
         """Returns self's name""" 
         return self.name
         
+class MITPerson(Person):
+    
+    nextIdNum = 0 #identification number
+    
+    def __init__(self, name):
+        Person.__init__(self, name)
+        self.idNum = MITPerson.nextIdNum 
+        MITPerson.nextIdNum += 1
+        
+    def getIdNum(self): 
+        return self.idNum
+        
+    def __lt__(self, other):
+        return self.idNum < other.idNum
+        
         
 You = Person('Hamburg Hello')
+#p1 = MITPerson('Barbara Beaver')
+
+#print ( str(p1) + '\'s id number is ' + str(p1.getIdNum()))
+#'''str() method is found in MITPerson's superclass, Person. getIdNum() is
+#of course, found in the MITPerson class'''
+
+
+#p1 = MITPerson('Mark Guttag')
+#p2 = MITPerson('Billy Bob Beaver') 
+#p3 = MITPerson('Billy Bob Beaver') 
+#p4 = Person('Billy Bob Beaver')
+#
+#print ('p1 < p2 =', p1 < p2) 
+#print ('p3 < p2 =', p3 < p2 )
+#print ('p4 < p1 =', p4 < p1 )
+
+
+class Student(MITPerson):
+    pass
+
+class UG(Student):
+    def __init__(self, name, classYear):
+        MITPerson.__init__(self, name)
+        self.year = classYear 
+    def getClass(self):
+        return self.year
+        
+class Grad(Student): 
+    pass
+
+
+
